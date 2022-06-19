43d4545d81e0ca8c628f1cf74e86968181f1503b
Ankita <thakkarankita0105@.github.com>
2022-06-19 18:56:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var url = 'https://api.funtranslations.com/translate/yoda.json';
+var button = document.querySelector("#btn");
+
+function generateURL(text) {
+    return url + "?text=" + text;
+}
+
+function getTranlatedText(text, outputBox) {
+    fetch(generateURL(text))
+        .then((res) => res.json())
+        .then((text) => {
+            var outputText = text.contents.translated;
+            outputBox.innerText = outputText
+        })
+        .catch((err) => {
+            alert("somyhing went wrong!! try after some time")
+            console.log(err);
+        })
+}
+button.addEventListener('click', () => {
+    console.log("clicked")
+    var inputBox = document.querySelector('#input');
+    var outputBox = document.querySelector('#output');
+    var inputText = inputBox.value;
+    getTranlatedText(inputText, outputBox);
+});
